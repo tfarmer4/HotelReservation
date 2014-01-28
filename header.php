468136c372bd2b3a74e1e43bbb2f583e90b045ec
Tim Farmer <tfarmer4@gmail.com>
2014-01-27 18:38:18 -0600

    Added About.php, Template.php(used as a template file for new pages), and reservations.php. Just working out a shell of a website for us to start with.

--- header.php
+++ header.php
@@ -1,6 +1,18 @@
 <!Doctype HTML>
+<div id="login" align="right">
+<form method="post" action="login.php">
+<p align="right" style="color:white">Username:<input type="text" name="u_name" required/></p>
+
+<p align="right" style="color:white">Password:<input type="password" name="p_word" required/></p>
+<input type="submit" name="b_login" value="Login"/>
+<a href="register.php">Register New User</a></form>
+</div>
 <ul id="nav">
 <li><a href="index.php">Home</a></li>
 <li><a href="reservations.php">Reservations</a></li>
-<li><a href="about.php">About</a></li>
+<li><a href="About.php">About</a></li>
 </ul>
+
+
+
+
