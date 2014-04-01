2286d645cd983fa24fe1e5fdd1e556924ff99f5d
Tim Farmer <tfarmer4@gmail.com>
2014-04-01 14:15:08 -0500

    Admins can now manage user permissions

--- application/controllers/admin_home.php
+++ application/controllers/admin_home.php
@@ -10,7 +10,7 @@ class Admin_home extends CI_Controller {
 		
 		// load helpers
 		$this->load->helper('url');
-		
+		$this->load->helper('form');
 		
 		// load models
 		$this->load->model("admin_main_mdl");
@@ -28,5 +28,35 @@ class Admin_home extends CI_Controller {
 	public function index() {
 		
 		//$this->load->view('admin/templates/footer');
+	}
+	
+	public function changeUsers()
+	{
+		var_dump($_POST);
+		$query = $this->db->get('Users');
+		foreach($query->result() as $row)
+		{
+			$isAdmin = $this->input->post($row->uName, TRUE) ? $this->input->post($row->uName, TRUE) : NULL;
+			if($isAdmin == 'Admin_' . $row->uName)
+			{
+				if($row->isAdmin != true)
+				{
+					$data = array(
+						'isAdmin'=>$this->db->escape(true)
+					);
+
+					$this->db->where('userID', $row->userID);
+					$this->db->update('Users', $data);
+					continue;
+				}
+			}
+			elseif ($isAdmin == 'User_'. $row->uName)
+			{
+				$data = array('isAdmin'=>$this->db->escape(false));
+				$this->db->where('userID', $row->userID);
+				$this->db->update('Users', $data);
+			}
+		}
+		redirect('admin_home', 'refresh');
 	}
 }
--- application/views/admin_home.php
+++ application/views/admin_home.php
@@ -9,10 +9,36 @@
 <body>
 
 <div id="main">
-<h1>Template</h1>
+<h1>Admin Console</h1>
 <p>Under Construction!!!</p>
 <em>Change & Innovation</em>
-<p></p>
+<div id="users">
+	<?php echo form_open('admin_home/changeUsers');?>
+	<table id="user_tbl" border="1">
+	<?php $query = $this->db->get('Users');
+	echo '<tr><td>User Name</td><td>Admin</td></tr>';
+	foreach ($query->result() as $row)
+	{
+		$options = array(
+                  'Admin_' . $row->uName  => 'Admin',
+                  'User_' . $row->uName    => 'User',
+                );
+
+		
+		echo '<tr><td>' . $row->uName . '</td><td>';
+		if($row->isAdmin == true)
+			echo form_dropdown($row->uName, $options, 'Admin_' . $row->uName);
+		elseif($row->isAdmin == false)
+			echo form_dropdown($row->uName, $options, 'User_' . $row->uName);
+		echo '</td></tr>';
+		
+	}
+	?>
+	</table>
+	<?php
+	echo form_submit('submit_user_changes', 'Save Changes');
+	echo form_close();?>
+</div>
 </div>
 </body>
 </html>
